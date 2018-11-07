732d67fb5c949632c6e49e08e9a58c373ef82c0d
Paolo Gigante <pgigante@splunk.com>
2018-11-07 16:36:56 +0000

    Download the relevant dot file

--- setup.sh
+++ setup.sh
@@ -2,13 +2,18 @@
 
 declare -A FILES
 FILES=(["vimrc"]=~/.vimrc)
-#FILES+=(["bashrc"]=~/.bashrc)
+# Example of how to add a new dot file.
+#FILES+=(["FILENAME"]=FILEPATH_NOQUOTES)
 
 for FILE in ${!FILES[@]}; do
   echo "Setting up dot file for ${FILE}:"
   if [[ -f ${FILES[$FILE]} ]]; then
     echo "${FILES[$FILE]} already exists. Backup and overwrite?"
+    echo "Backup created on $(date)" >> ${FILES[$FILE]}
+    mv ${FILES[$FILE]} ${FILES[$FILE]}.old
+    wget https://raw.githubusercontent.com/reaperzn/dotfiles/master/${FILE} -O ${FILES[$FILE]}
   else
     echo "${FILES[$FILE]} does not exist: Creating new file"
+    wget https://raw.githubusercontent.com/reaperzn/dotfiles/master/${FILE} -O ${FILES[$FILE]}
   fi
 done
